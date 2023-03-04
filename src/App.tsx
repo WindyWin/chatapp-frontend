e41be9e29e3bd80648fd112d08531c7497bf25a9
WindyWind <19521362@gm.uit.edu.vn>
2023-03-04 16:03:29 +0700

    snack noti package

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Skeleton } from "@mui/material";
+import { SnackbarProvider } from "notistack";
 import { StrictMode, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { ErrorBoundary } from "react-error-boundary";
 import { RouterProvider } from "react-router-dom";
 import ErrorFallback from "./component/ui/ErrorFallback";
+import LoadingFallback from "./component/ui/LoadingFallback";
 import "./config/axios";
 import "./config/firebase";
 import router from "./modules/router";
@@ -14,16 +16,18 @@ import "./_reset.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ErrorBoundary
-      //when error, show ErrorFallback component
-      fallback={<ErrorFallback />}
-      onReset={() => window.location.reload()}
-    >
-      <Suspense
-        //when loading show Skeleton component
-        fallback={<Skeleton />}>
-        <RouterProvider router={router}></RouterProvider>
-      </Suspense>
-    </ErrorBoundary>
+    <SnackbarProvider maxSnack={3}>
+      <ErrorBoundary
+        //when error, show ErrorFallback component
+        fallback={<ErrorFallback />}
+        onReset={() => window.location.reload()}
+      >
+        <Suspense
+          //when loading show Skeleton component
+          fallback={<LoadingFallback />}>
+          <RouterProvider router={router}></RouterProvider>
+        </Suspense>
+      </ErrorBoundary>
+    </SnackbarProvider>
   </StrictMode>
 );
