507db1d17d46b22cbeeb6f9a5e95f90c81424a5e
WindyWind <19521362@gm.uit.edu.vn>
2023-03-22 16:50:49 +0700

    update user service

--- src/modules/page/User/Profile.tsx
+++ src/modules/page/User/Profile.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { useParams } from 'react-router-dom'
+import MyProfile from '../../../component/core/MyProfile'
 
 function Profile() {
+    const param = useParams()
+
     return (
-        <div>Profile</div>
+        <MyProfile uid={param.uid} />
     )
 }
 
--- src/service/userService.tsx
+++ src/service/userService.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { getDownloadURL, getStorage, ref, uploadString } from 'firebase/storage';
 const register = async ({ uid, email, username, password }:
     { uid: string, email: string, username: string, password: string }) => {
     return axios.post('/users/register', { uid, email, username, password });
@@ -16,7 +17,20 @@ const checkExistUser = ({ email, username }: any) => {
 const getUserByUid = (uid: string) => {
     return axios.get(`users?uid=${uid}`)
 }
+const updateUserAvatar = async ({ uid, avatar }: { uid: string, avatar: string }) => {
+    const storage = getStorage();
+    //random name for image
+    const storageRef =
+        ref(storage, `avatar/${uid}`);
 
+    const snapsot = await uploadString(storageRef, `${avatar}`, 'data_url')
+    const dowloadURL = await getDownloadURL(snapsot.ref);
+    avatar = dowloadURL;
+    return axios.put(`users/avatar`, { uid, avatar });
+}
+const updateUserUsername = ({ uid, oldUsername, username }: { uid: string, oldUsername: string, username: string }) => {
+    return axios.put(`users/username`, { uid, oldUsername, username })
+}
 
-export { register, login, searchUser, checkExistUser, getUserByUid };
+export { register, login, searchUser, checkExistUser, getUserByUid, updateUserAvatar, updateUserUsername };
 
